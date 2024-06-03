8cd0f5b31658efe615cce3ec68a1db54fccb4822
Rene Couturier <rRene333@googlemail.com>
2024-06-03 15:56:34 +0200

    rdy

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,92 @@
-import PizZip from "pizzip";
-import { useRef, useState } from "react";
-import { Input } from "./components/ui/input";
+import { useEffect, useState } from "react";
 import * as XLSX from "xlsx";
 import { Button } from "./components/ui/button";
-import { Loader2 } from "lucide-react";
+import Bieter from "./Bieter";
+import { Save, Trash } from "lucide-react";
+import { cn } from "./lib/utils";
+import { Input } from "./components/ui/input";
 
-function str2xml(str: string) {
-  if (str.charCodeAt(0) === 65279) {
-    // BOM sequence
-    str = str.substring(1);
-  }
-  return new DOMParser().parseFromString(str, "text/xml");
+
+const newArray = (data: any[]) => {
+  let finalArray = [] as any[];
+
+  data.forEach((d: any) => {
+    const name = d.name;
+    const bieterData = d.data
+
+    finalArray.push(...bieterData.map((data: any) => ({teilnehmer: name, ...data})))
+
+
+  });
+
+  return finalArray;
+
 }
 
-const exportData = (data: any[], bieter: string) => {
-  const worksheet = XLSX.utils.json_to_sheet(data);
+
+const exportData = (data: any[], fileName: string) => {
+  
+
+  let finalArray = newArray(data);
+
+  console.log({
+    first: finalArray,
+    second: data[0].data
+  });
+
   const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, worksheet, bieter);
+  const worksheet = XLSX.utils.json_to_sheet(finalArray);
+
+  XLSX.utils.book_append_sheet(workbook, worksheet, fileName);
 
   XLSX.writeFile(
     workbook,
-    `${new Date().toLocaleDateString("de-DE")}-${bieter}.xlsx`,
+    `${new Date().toLocaleDateString("de-DE")}-${fileName}.xlsx`,
     { compression: true }
   );
 };
 
-const parseValue = (value: string | null) => {
-  switch (value) {
-    case "☐":
-      return false;
-    case "☒":
-      return true;
-
-    default:
-      return value;
-  }
-};
-
-function getElements(content: any) {
-  if (!content) {
-    return [];
-  }
-  const zip = new PizZip(content);
-  const xml = str2xml(zip.files["word/document.xml"].asText());
-
-  const richs = xml.getElementsByTagName("w:sdt");
-  const allRichs = [];
-
-  for (let i = 0, len = richs.length; i < len; i++) {
-    const rich = richs[i];
-    const title = rich.getElementsByTagName("w:alias")[0]?.getAttribute("w:val");
-    const tag = rich.getElementsByTagName("w:tag")[0]?.getAttribute("w:val");
-    const value = rich.getElementsByTagName("w:t")[0]?.childNodes[0].nodeValue;
-    allRichs.push({ Titel: title, Tag: tag, Wert: parseValue(value) });
-  }
-
-  const countSubTags = allRichs.map((rich) => rich.Tag ?  rich.Tag.split("|").length - 1 : 0);
-
-  const maxSubTags = Math.max(...countSubTags);
-
-  const newRichs = allRichs.map((rich) => {
-
-    const newRich = {...rich} as any;
-
-    const subTags = rich.Tag?.split("|");
-
-    if (subTags) {
-      newRich.Tag = subTags[0];
-    }
-
-    for (let i = 0; i < maxSubTags; i++) {
-      newRich[`Tag ${i + 1}`] = subTags && subTags[i + 1] ? subTags[i + 1] : "";
-    }
-
-    return newRich;
-
-  })
-
-  return newRichs;
-}
-
 function App() {
-  const [data, setData] = useState<any>(null);
-  const [bieter, setBieter] = useState<string>("");
-  
-  const formRef = useRef<HTMLFormElement>(null);
+  const [dataArray, setDataArray] = useState<any[]>([]);
+  const [fileName, setFileName] = useState<string>("");
 
-  const onFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-    let file = event.target.files?.[0]; // Add null check for event.target.files
+  useEffect(() => {
+    console.log("dataArray", dataArray);
+  }, [dataArray]);
 
-    reader.onload = (e) => {
-      const content = e.target?.result;
-      const elements = getElements(content);
-      console.log(elements);
-      setData(elements);
-    };
-
-    reader.onerror = (err) => console.error(err);
-
-    if (file) {
-      reader.readAsArrayBuffer(file);
-    }
+  const deleteData = (name: string) => {
+    setDataArray(dataArray.filter((data) => data.name !== name));
   };
 
   return (
-    <>
-      <div className="w-full h-screen flex items-center justify-center">
-        {data ? (
-          <form ref={formRef} className="flex flex-col space-y-2">
-            <Input
-              onChange={(e) => setBieter(e.target.value)}
-              placeholder="Bieter angeben"
-            />
-            <Button onClick={() => exportData(data, bieter)}>
-              Exportieren
+    <div className="w-full flex flex-col space-y-10 items-center h-screen justify-center">
+      <Bieter setDataArray={setDataArray} />
+      <div className="container grid grid-cols-8 h-[60px] gap-4">
+        {dataArray.map((data, index) => (
+          <div
+            key={index}
+            className="border-2 p-4 flex flex-col items-center rounded-md"
+          >
+            <h2 className="text-md mb-2">{data.name}</h2>
+            <Button onClick={() => deleteData(data.name)}>
+              <Trash className="size-4 mr-2" />
+              Löschen
             </Button>
-            <Button onClick={()=>formRef.current?.reset()} >
-              <Loader2 className="size-4 mr-2" /> Reload
-            </Button>
-          </form>
-        ) : (
-          <div className="">
-            <Input className="" onChange={onFileUpload} type="file" />
           </div>
-        )}
+        ))}
       </div>
-    </>
+      <div className={cn("hidden space-x-2 bg-accent p-3 rounded-md", dataArray.length != 0 && "flex")}>
+        <Input placeholder="Dateiname" onChange={(e) => setFileName(e.target.value)} />
+        <Button
+          variant={"outline"}
+          disabled={fileName === ""}
+          onClick={() => exportData(dataArray, fileName)}
+        >
+          <Save className="size-4 mr-2" />
+          Exportieren
+        </Button>
+      </div>
+    </div>
   );
 }
 
---
+++ src/Bieter.tsx
@@ -0,0 +1,150 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "./components/ui/button";
+import { Plus } from "lucide-react";
+import { Input } from "./components/ui/input";
+import { useState } from "react";
+import PizZip from "pizzip";
+
+function str2xml(str: string) {
+  if (str.charCodeAt(0) === 65279) {
+    // BOM sequence
+    str = str.substring(1);
+  }
+  return new DOMParser().parseFromString(str, "text/xml");
+}
+
+const parseValue = (value: string | null) => {
+  switch (value) {
+    case "☐":
+      return false;
+    case "☒":
+      return true;
+
+    default:
+      return value;
+  }
+};
+
+function getElements(content: any) {
+  if (!content) {
+    return [];
+  }
+  const zip = new PizZip(content);
+  const xml = str2xml(zip.files["word/document.xml"].asText());
+
+  const richs = xml.getElementsByTagName("w:sdt");
+  const allRichs = [];
+
+  for (let i = 0, len = richs.length; i < len; i++) {
+    const rich = richs[i];
+    const title = rich
+      .getElementsByTagName("w:alias")[0]
+      ?.getAttribute("w:val");
+    const tag = rich.getElementsByTagName("w:tag")[0]?.getAttribute("w:val");
+    const value = rich.getElementsByTagName("w:t")[0]?.childNodes[0].nodeValue;
+    allRichs.push({ Titel: title, Tag: tag, Wert: parseValue(value) });
+  }
+
+  const countSubTags = allRichs.map((rich) =>
+    rich.Tag ? rich.Tag.split("|").length - 1 : 0
+  );
+
+  const maxSubTags = Math.max(...countSubTags);
+
+  const newRichs = allRichs.map((rich) => {
+    const newRich = { ...rich } as any;
+
+    const subTags = rich.Tag?.split("|");
+
+    if (subTags) {
+      newRich.Tag = subTags[0];
+    }
+
+    for (let i = 0; i < maxSubTags; i++) {
+      newRich[`Tag ${i + 1}`] = subTags && subTags[i + 1] ? subTags[i + 1] : "";
+    }
+
+    return newRich;
+  });
+
+  return newRichs;
+}
+
+function Bieter({ setDataArray }: { setDataArray: any }) {
+
+    const [open, setOpen] = useState(false);
+
+  const [name, setName] = useState("");
+
+  const [bieterData, setBieterData] = useState<any>(null);
+
+  const onFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    let file = event.target.files?.[0]; // Add null check for event.target.files
+
+    reader.onload = (e) => {
+      const content = e.target?.result;
+      const elements = getElements(content);
+      setBieterData(elements);
+    };
+
+    reader.onerror = (err) => console.error(err);
+
+    if (file) {
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const onSubmit = () => {
+    setDataArray((state: any[]) => [
+      ...state,
+      { name, data: bieterData },
+    ]);
+    setOpen(false);
+  };
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button size={"sm"}>
+          <Plus className="size-6 mr-2" /> Teilnehmer hinzufügen
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Input className="" onChange={onFileUpload} type="file" />
+            <Input
+              placeholder="Name Teilnehmer"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Button
+              disabled={name === "" || bieterData === null}
+              onClick={() => onSubmit()}
+            >
+              Hinzufügen
+            </Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Abbrechen</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+export default Bieter;
